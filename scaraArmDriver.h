01955021c6074e3be13993a1c2c73a4af1139c95
rgabbai <rony.gabbai@gmail.com>
2024-07-01 15:58:28 +0300

    fix arm length constants

--- scaraArmDriver.h
+++ scaraArmDriver.h
@@ -1,3 +1,3 @@
-const float L1 = 125.0; // [mm]Length of the first segment
-const float L2 = 90.0; // [mm]Length of the second segment
+const float L1 = 90.0; // [mm]Length of the first segment
+const float L2 = 125.0; // [mm]Length of the second segment
 
